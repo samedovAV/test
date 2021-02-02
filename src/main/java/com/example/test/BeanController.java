bf7c39e877406b879f7ae1d76a2e8868bccfac05
Aleksandr Samedov <samedov@advantum.pro>
2021-02-02 11:48:40 +0300

    add

--- src/main/java/com/example/test/BeanController.java
+++ src/main/java/com/example/test/BeanController.java
@@ -1,14 +1,16 @@
 package com.example.test;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 public class BeanController {
 
-    BeanService beanService;
+    private final BeanService beanService;
 
     @GetMapping(value = "/get")
     public List<BeanB> getAllBeans() {
