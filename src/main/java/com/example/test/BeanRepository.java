5abffd02aa20d8cab97c183a58f6ba2e66629451
Aleksandr Samedov <samedov@advantum.pro>
2021-02-02 15:21:10 +0300

    add get parameter in path

--- src/main/java/com/example/test/BeanRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.test;
-
-import org.springframework.stereotype.Repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-@Repository
-public interface BeanRepository extends JpaRepository<BeanB, Long> {
-
-}
